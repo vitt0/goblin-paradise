ef27cf42f5228e685699ff7738ca529e3f47d899
vitt0 <vitt0ne.jc@gmail.com>
2023-07-07 03:44:22 -0300

    Create Scene.js

---
+++ public/Scene.js
@@ -0,0 +1,12 @@
+class ExternalScene extends window.BaseScene {
+  constructor() {
+    super({
+      player: {
+        spawn: {
+          x: 210,
+          y: 280,
+        },
+      },
+    });
+  }
+}
