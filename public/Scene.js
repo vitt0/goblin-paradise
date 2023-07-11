2da42a7019c12568e49fbc46b0f9904da8fcdb3d
vitt0 <vitt0ne.jc@gmail.com>
2023-07-11 03:48:09 -0300

    Update Scene.js

--- public/Scene.js
+++ public/Scene.js
@@ -8,5 +8,57 @@ class ExternalScene extends window.BaseScene {
         },
       },
     });
+
+    // Projectile types
+    this.blueberryProjectile = {
+      speed: 500,       // Fast speed
+      size: 10,         // Small size
+      damage: 0.25,     // Damage value
+    };
+
+    this.orangeProjectile = {
+      speed: 300,       // Medium speed
+      size: 20,         // Medium size
+      damage: 0.3,      // Damage value
+    };
+
+    this.appleProjectile = {
+      speed: 100,       // Slow speed
+      size: 30,         // Big size
+      damage: 0.35,     // Damage value
+    };
+
+  create() {
+    super.create();
+
+    const npcClothing = {
+      body: "Goblin Potion",
+      hat: "Warrior Helmet",
+      hair: "Buzz Cut",
+      shirt: "Warrior Shirt",
+      pants: "Warrior Pants",
+      tool: "Ancient Goblin Sword",
+    };
+
+    this.initialiseNPCs([
+      {
+        x: 240,
+        y: 180,
+        npc: "Test",
+        clothing: npcClothing,
+        onClick: () => {
+          window.openModal({
+            npc: {
+              name: "Test",
+              clothing: npcClothing,
+            },
+            jsx: "Shot me if you can!",
+          });
+        },
+      },
+    ]);
+    
   }
+
+  // Other methods and game logic...
 }
